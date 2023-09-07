5074da5ef608f62f940385582e6433493002d29b
manlikevee <odahviktor@gmail.com>
2023-09-07 15:43:14 +0100

    ve

---
+++ src/components/Home/Home.js
@@ -0,0 +1,252 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+<div className="wrapper detail-page">
+  <div className="main-content">
+    <br />
+    <div className="toptextdata">Welcome Vee!</div>
+    <div className="assenttext">Here is what we have for you today</div>
+    <div className="dashboardintroduction">
+      <div className="dashboarddatacard">
+        <div className="dbicons submit">
+          <span className="material-symbols-outlined">data_table</span>
+        </div>
+        <div className="dbtextdata">
+          <div className="dbtitle">30</div>
+          <div className="dbsubtext">Submitted application</div>
+        </div>
+      </div>
+      <div className="dashboarddatacard rolematch">
+        <div className="dbicons match">
+          <span className="material-symbols-outlined">mediation</span>
+        </div>
+        <div className="dbtextdata">
+          <div className="dbtitle">30</div>
+          <div className="dbsubtext">Role Match</div>
+        </div>
+      </div>
+      <div className="dashboarddatacard bookmark">
+        <div className="dbicons mysave">
+          <span className="material-symbols-outlined">bookmarks</span>
+        </div>
+        <div className="dbtextdata">
+          <div className="dbtitle">30</div>
+          <div className="dbsubtext">Saved application</div>
+        </div>
+      </div>
+    </div>
+    <br />
+    <br />
+    <br />
+    <div className="veedbflex">
+      <div className="dbflexsideone">
+        <div className="job-card-title dbd">
+          Job Alerts
+          <div className="containerflex">
+            <div className="jmini">Monitor artist sales, reviews, etc.</div>
+            <div className="forminput">
+              <input
+                type="email"
+                placeholder="Search for Job"
+                name="username"
+                id="id_username"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="jobssection">
+          <div className="dbjobscard">
+            <div className="cardcolumntop">
+              <div className="dbcompanylogo">
+                <img
+                  src="https://nigerialogos.com/logos/omenka/omenka.svg"
+                  alt=""
+                />
+              </div>
+              <div className="dbcompanylocation">
+                <div className="spanicon">
+                  <span className="material-symbols-outlined">pin_drop</span>
+                </div>
+                <div className="local">Abuja, Nigeria</div>
+              </div>
+            </div>
+            <div className="cardcolumntitle">
+              <div className="dbjobservice">Financial Services</div>
+              <div className="dbjobtitle">Risk Consulting Director</div>
+            </div>
+            <div className="cardcolumnbody">
+              <div className="job-card-subtitle bigs">
+                victo Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Porro, ipsum est veniam pariatur tempora modi molestias mollitia
+                deleniti distinctio minima voluptatibus alias dolore corrupti
+                molestiae. Nobis facere dolorem tempora accusamus modi, commodi
+                sit amet eos voluptates soluta provident facilis ab suscipit!
+                Explicabo sequi quo amet dolor, quam nesciunt facere error!
+              </div>
+            </div>
+            <div className="dbtags">
+              <div className="job-detail-buttons">
+                <button className="search-buttons detail-button">
+                  Full Time
+                </button>
+                <button className="search-buttons detail-button">
+                  Min. 1 Year
+                </button>
+                <button className="search-buttons detail-button">
+                  Senior Level
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="dbjobscard">
+            <div className="cardcolumntop">
+              <div className="dbcompanylogo">
+                <img
+                  src="https://nigerialogos.com/logos/seedbuilders/seedbuilders.png"
+                  alt=""
+                />
+              </div>
+              <div className="dbcompanylocation">
+                <div className="spanicon">
+                  <span className="material-symbols-outlined">pin_drop</span>
+                </div>
+                <div className="local">Abuja, Nigeria</div>
+              </div>
+            </div>
+            <div className="cardcolumntitle">
+              <div className="dbjobservice">Financial Services</div>
+              <div className="dbjobtitle">Risk Consulting Director</div>
+            </div>
+            <div className="cardcolumnbody">
+              <div className="job-card-subtitle bigs">
+                victo Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Porro, ipsum est veniam pariatur tempora modi molestias mollitia
+                deleniti distinctio minima voluptatibus alias dolore corrupti
+                molestiae. Nobis facere dolorem tempora accusamus modi, commodi
+                sit amet eos voluptates soluta provident facilis ab suscipit!
+                Explicabo sequi quo amet dolor, quam nesciunt facere error!
+              </div>
+            </div>
+            <div className="dbtags">
+              <div className="job-detail-buttons">
+                <button className="search-buttons detail-button">
+                  Full Time
+                </button>
+                <button className="search-buttons detail-button">
+                  Min. 1 Year
+                </button>
+                <button className="search-buttons detail-button">
+                  Senior Level
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="dbjobscard">
+            <div className="cardcolumntop">
+              <div className="dbcompanylogo">
+                <img
+                  src="https://nigerialogos.com/logos/serah_kassim/serah_kassim.png"
+                  alt=""
+                />
+              </div>
+              <div className="dbcompanylocation">
+                <div className="spanicon">
+                  <span className="material-symbols-outlined">pin_drop</span>
+                </div>
+                <div className="local">Abuja, Nigeria</div>
+              </div>
+            </div>
+            <div className="cardcolumntitle">
+              <div className="dbjobservice">Financial Services</div>
+              <div className="dbjobtitle">Risk Consulting Director</div>
+            </div>
+            <div className="cardcolumnbody">
+              <div className="job-card-subtitle bigs">
+                victo Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Porro, ipsum est veniam pariatur tempora modi molestias mollitia
+                deleniti distinctio minima voluptatibus alias dolore corrupti
+                molestiae. Nobis facere dolorem tempora accusamus modi, commodi
+                sit amet eos voluptates soluta provident facilis ab suscipit!
+                Explicabo sequi quo amet dolor, quam nesciunt facere error!
+              </div>
+            </div>
+            <div className="dbtags">
+              <div className="job-detail-buttons">
+                <button className="search-buttons detail-button">
+                  Full Time
+                </button>
+                <button className="search-buttons detail-button">
+                  Min. 1 Year
+                </button>
+                <button className="search-buttons detail-button">
+                  Senior Level
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="dbsideflextwo">
+        <div className="messagingbox">
+          <div className="msgtitle">
+            <div className="job-card-title dbd">Messages</div>
+            <div className="jmini">Discover Opportunities..</div>
+          </div>
+          <div className="messagingflow">
+            <div className="dbmessagingbox">
+              <div className="messagingboxicon">
+                <img
+                  src="https://i.pinimg.com/564x/f8/25/2a/f8252af763f0bb3b53a0cb8477f80711.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="dbmessagingtext">
+                <div className="job-card-title">Victor Odah</div>
+                <div className="jmini">
+                  victor odah ebube dbd d Lorem ipsum dolor sit.
+                </div>
+              </div>
+            </div>
+            <div className="dbmessagingbox">
+              <div className="messagingboxicon">
+                <img
+                  src="https://i.pinimg.com/564x/37/ef/13/37ef13b5f1d38d72b01ff07675b4d8bd.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="dbmessagingtext">
+                <div className="job-card-title">Victor Odah</div>
+                <div className="jmini">
+                  victor odah ebube dbd d Lorem ipsum dolor sit.
+                </div>
+              </div>
+            </div>
+            <div className="dbmessagingbox">
+              <div className="messagingboxicon">
+                <img
+                  src="https://i.pinimg.com/564x/00/29/93/002993ca60ccc2ad63e90d60c019163f.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="dbmessagingtext">
+                <div className="job-card-title">Victoria justice</div>
+                <div className="jmini">
+                  victor odah ebube dbd d Lorem ipsum dolor sit.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+    </div>
+  )
+}
+
+export default Home
