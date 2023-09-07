4ec7584a847c4d4e615eaf8c81393f24c66fca29
manlikevee <odahviktor@gmail.com>
2023-09-07 15:47:56 +0100

    veee

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Header from '../components/Header/Header'
+import Home from '../components/Home/Home'
 import Layout from '../components/Layout/Layout'
 const index = () => {
   return (
     <>
   <Layout>
-
+<Home/>
     </Layout>
     </>
   )
