21667ce82122557d992c32f4700ac1a9b11e8ce2
manlikevee <odahviktor@gmail.com>
2023-09-07 15:41:46 +0100

    vesc

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -23,36 +23,28 @@ const Header = () => {
     <div className="header">
     <div className="logo">
       <Link to='/' style={{ color: "inherit", textDecoration: "none" }}>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-          <path
-            xmlns="http://www.w3.org/2000/svg"
-            d="M512 503.5H381.7a48 48 0 01-45.3-32.1L265 268.1l-9-25.5 2.7-124.6L338.2 8.5l23.5 67.1L512 503.5z"
-            fill="#0473ff"
-            data-original="#28b446"
-          />
-          <path
-            xmlns="http://www.w3.org/2000/svg"
-            fill="#0473ff"
-            data-original="#219b38"
-            d="M361.7 75.6L265 268.1l-9-25.5 2.7-124.6L338.2 8.5z"
-          />
-          <path
-            xmlns="http://www.w3.org/2000/svg"
-            d="M338.2 8.5l-82.2 234-80.4 228.9a48 48 0 01-45.3 32.1H0l173.8-495h164.4z"
-            fill="#0473ff"
-            data-original="#518ef8"
-          />
-        </svg>
-        Milao
+      <img src="https://nigerialogos.com/logos/seedbuilders/seedbuilders.png" alt="" className='myllogo' />
       </Link>
     </div>
     
     <div className={`header-menu ${navclicked ? 'navactive' : 'header-menu'}`}>
       <Link to='/Jobs'  className="active">
-        Find Job
+         
+      <span class="material-symbols-outlined">
+    space_dashboard
+    </span>
+     Dashboard
       </Link>
-      <Link to='/Jobdetail'>Company Review</Link>
-      <Link to='/Messaging'>Find Salaries</Link>
+      <Link to='/Jobdetail'>
+        <span class="material-symbols-outlined">
+    work_update
+    </span>All Jobs </Link>
+      <Link to='/Messaging'><span class="material-symbols-outlined">
+    work_history
+    </span> Applications</Link>
+    <Link to='/Messaging'><span class="material-symbols-outlined">
+    account_box
+    </span> Profile</Link>
     </div>
     <div className="user-settings">
       <div className="dark-light" onClick={addClassNameToBody}>
