5cf665a83c8121ec2cc2a9895e715d1cc06b7569
Crux <olam4461@gmail.com>
2023-09-07 15:52:49 +0100

    nf

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,22 @@
 import React, { useEffect } from 'react'
 import './Style.css'
 import Header from '../Header/Header'
+import { Helmet } from 'react-helmet';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Layout = ({ children }) => {
   return (
     <div>
+              <Helmet>
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+  
+
+    {/* Add more head tags here */}
+  </Helmet>  
         <Header/>
+        <ToastContainer/>
 
         <div className="job">
         {children}
